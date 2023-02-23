d4d495c2e4af60ade7948fac650ab0d34f77f0cd
Teruto Sato <211052@st.yohida-g.ac.jp>
2023-02-24 08:59:45 +0900

    Release

--- enemy.cpp
+++ enemy.cpp
@@ -122,7 +122,7 @@ HRESULT CEnemy::Init(D3DXVECTOR3 pos)
 	//初期値の設定
 	m_pos = pos;
 	m_nCntMotion = 1;
-	m_fLife = 8000.0f;			//体力
+	m_fLife = 6500.0f;			//体力
 	m_fRemLife = 100.0f;		//残り体力(%)
 	m_fMaxLife = m_fLife;		//最大体力
 	m_fGravity = fDefGravity;	//重力の値
